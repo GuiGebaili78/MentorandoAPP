322da3d7a7a1dcb493a41ad810095a94a4c9b3f7
GuiGebaili78 <103890957+GuiGebaili78@users.noreply.github.com>
2024-03-21 15:10:16 -0300

    first commit

---
+++ app/src/main/java/br/com/fiap/mentorandoapp/ui/theme/Color.kt
@@ -0,0 +1,23 @@
+package br.com.fiap.mentorandoapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+
+
+//MentorandoAPP paleta de cores
+
+val color1 = Color(0xFFC5F1FA)
+val color2 = Color(0xFFAFCACF)
+val color3 = Color(0xFF709CA5)
+val color4 = Color(0xFF3E707A)
+val color5 = Color(0xFF1C4952)
+val color6 = Color(0xFF0F3138)
+
